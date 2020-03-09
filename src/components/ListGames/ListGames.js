7c69c911f63ce16b2db1325d3167bfd594b7f232
eugferman <eugferman@gmail.com>
2020-03-09 09:46:16 +0100

    media responsive added

--- src/components/ListGames/ListGames.js
+++ src/components/ListGames/ListGames.js
@@ -1,30 +1,29 @@
 import React from "react";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
-import './ListGames.scss';
+import "./ListGames.scss";
 
 function ListGames() {
-
   const gamesRedux = useSelector(state => state.games);
-  const gamesSorted = gamesRedux.sort((a, b) => a.score - b.score);
+  const gamesSorted = gamesRedux.sort((a, b) => b.score - a.score);
 
   return (
     <div className="main-list-games">
       <div className="list-games">
         <ul>
-          {gamesSorted.map(listGames => { 
-            return  (
-            <li data-testid={listGames.id} key={listGames.id}>
-              <p>{listGames.name}</p>
-              <p>Score: {listGames.score}</p>
-              <img src={listGames.cover} alt={listGames.name}></img>
-            </li>
-          )})}
+          {gamesSorted.map(listGames => {
+            return (
+              <li data-testid={listGames.id} key={listGames.id}>
+                <p>{listGames.name}</p>
+                <p>Score: {listGames.score}</p>
+                <img src={listGames.cover} alt={listGames.name}></img>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default ListGames;
